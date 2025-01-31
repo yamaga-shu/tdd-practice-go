a1a6eda079e83f212021a562d6bd715e36b757d9
shu.yamaga <s.yamaga.0318@gmail.com>
2025-01-31 21:09:18 +0900

    第二章 明白な実装

--- money/money_test.go
+++ money/money_test.go
@@ -3,16 +3,22 @@ package money
 import "testing"
 
 func TestMultiplication(t *testing.T) {
-	cases := []struct {
-		in, multiplier, want int
-	}{
-		{2, 5, 10},
+	five := &Dollar{5}
+	product := five.Times(2)
+
+	got := product.amount
+	want := 10
+
+	if got != want {
+		t.Errorf("five.Times(2) == %d, want %d", got, want)
 	}
 
-	for _, c := range cases {
-		got := (&Dollar{c.in}).Times(c.multiplier).amount
-		if got != c.want {
-			t.Errorf("(&Dollar{%d}).Times(%d).amount == %d, want %d", c.in, c.multiplier, got, c.want)
-		}
+	product = five.Times(3)
+
+	got = product.amount
+	want = 15
+
+	if got != want {
+		t.Errorf("five.Times(3) == %d, want %d", got, want)
 	}
 }
